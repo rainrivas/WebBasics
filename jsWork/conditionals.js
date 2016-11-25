c93c2b519ed7072ceec7501d951b6807ced85cce
Rain <rain.rivas@gmail.com>
2016-11-24 22:02:35 -0700

    Conditional basic example

---
+++ jsWork/conditionals.js
@@ -0,0 +1,19 @@
+age = prompt("What is your age sessy pants");
+// Negative age
+if (age<0){
+	console.log("come back when you're not a nothing");
+}
+// Age 21
+if (age === 21){
+	console.log("ooo shizz free shot for you");
+}
+
+// Odd age
+if (age % 2 != 0){
+	console.log("aren't you rather odd");
+}
+
+// Perfect Square
+if(age % Math.sqrt(age)===0){
+	console.log("baby you're perfect ... square");
+}
