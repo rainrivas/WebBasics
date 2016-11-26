14155e19d2017828021e01859e2b58303f03aa9a
Rain <rain.rivas@gmail.com>
2016-11-26 12:35:42 -0700

    Cleaning toDo.js for readability.

--- jsWork/toDo.js
+++ jsWork/toDo.js
@@ -5,26 +5,40 @@ var input = (prompt("What would you like to do?")).toLowerCase();
 while (input != 'quit') {
     // list block
     if (input === 'list') {
-        console.log("***********");
-        todos.forEach(function(currentElement, indexOfElem) {
-            console.log(indexOfElem + ": " + currentElement);
-        });
-        console.log("***********");
+        listToDo();
     }
     // new block
     else if (input === 'new') {
         // ask for new to do and add to array
-        var newTodo = prompt('Enter new to do');
-        todos.push(newTodo);
-        console.log("item Added!");
+        newToDo();
+        listToDo();
     }
     // delete block
     else if (input === 'delete') {
-        var indexDel = prompt("Enter index of to do");
-        todos.splice(indexDel, 1);
-        console.log("item Deleted!");
+        deleteItem();
+        listToDo();
     }
     // more input
     input = (prompt("What would you like to do?")).toLowerCase();
 }
 console.log("All Done");
+
+function listToDo() {
+    console.log("***********");
+    todos.forEach(function(currentElement, indexOfElem) {
+        console.log(indexOfElem + ": " + currentElement);
+    });
+    console.log("***********");
+}
+
+function newToDo() {
+    var newTodo = prompt('Enter new to do');
+    todos.push(newTodo);
+    console.log("Item Added!\nNew List:");
+}
+
+function deleteItem() {
+    var indexDel = prompt("Enter index of to do");
+    todos.splice(indexDel, 1);
+    console.log("item Deleted!\nNew List:");
+}
