72602e1ffeaa7f1485421677cb6ee652b05f9fbf
Rain <rainrivas@gmail.com>
2016-11-27 19:03:19 -0700

    Adding the opitimzed code snippet to avoid multiple console.logs

--- jsWork/movieDb.js
+++ jsWork/movieDb.js
@@ -29,9 +29,21 @@ var movies = [
 movies.forEach(function (currentMovie) {
 	if(currentMovie.watched) {
 		if(currentMovie.rating <2) {
-			console.log("You have watched \""+currentMovie.title+"\" -- Your Rating: "+currentMovie.rating+" star");
+			console.log("You have watched \"" + currentMovie.title + "\" -- Your Rating: " + currentMovie.rating + " star");
 		}
-		else console.log("You have watched \""+currentMovie.title+"\" -- Your Rating: "+currentMovie.rating+" stars");
+		else console.log("You have watched \"" + currentMovie.title + "\" -- Your Rating: " + currentMovie.rating + " stars");
 	}
-	else console.log("You have not watched \""+currentMovie.title+"\" -- Your Rating: N/A");
+	else console.log("You have not watched \"" + currentMovie.title + "\" -- Your Rating: N/A");
 });
+
+// Optimized solution 
+/*
+var result = "You have ";
+if (currentMovie.hasWatched){
+	result+="watched ";
+} else {
+	result += "not watched ";
+}
+result +="\"" + currentMovie.title + "\" -- ";
+result += currentMovie.rating + " stars"; // optimized this code piece here to include the fact that I'm using if to count for 1 star
+*/
