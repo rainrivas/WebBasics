4bf4082e473a88d1dacb63e52665cfb75721a79d
Rain <rain.rivas@gmail.com>
2016-12-03 00:27:23 -0700

    WIP initial upload of color game base files with project criteria

---
+++ colorGame/colorGame.js
@@ -0,0 +1,7 @@
+// Temporary decorative js to show it works
+document.querySelector("h1").addEventListener("mouseover",function(){
+	this.style.color="black";
+});
+document.querySelector("h1").addEventListener("mouseout",function(){
+	this.style.color="white";
+});
