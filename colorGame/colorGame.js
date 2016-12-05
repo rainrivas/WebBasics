ccf64a8fa9add3419f1ebd4d04f6c1eb90c0dfe1
Rain <rainrivas@gmail.com>
2016-12-04 20:19:09 -0700

    Game is in working state. There are bugs, but those will be addressed in optimization pushes to come. KNOWN ISSUES: (1)"WIN" label present until "new game" is pressed despite that easy/hard buttons load new colors, they don't clear the winning label. (2) the menu items will disappear after being clicked unless you are hovering or you click somewhere else. (3) the game does not end after guessing correctly; you can still click on other color tiles and they will disappear.

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -7,13 +7,18 @@ document.querySelector("h1").addEventListener("mouseout", function() {
 });
 
 var newColors = document.querySelector("#new")
+var easy = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
 var colorTiles = document.querySelectorAll(".square");
 var currentRgb = document.querySelector("#currentRgb");
+var winLabel = document.getElementsByTagName("H2")[0];
 var answerRgb;
+var difficulty = 6; // use for answer setting, defaults to hard (6 tiles)
+
 function changeColor() {
     var r, g, b, colorRgb, min, max;
     min = Math.ceil(0);
-    max = Math.floor(255);
+    max = Math.floor(256);
     r = Math.floor(Math.random() * (max - min)) + min;
     g = Math.floor(Math.random() * (max - min)) + min;
     b = Math.floor(Math.random() * (max - min)) + min;
@@ -23,29 +28,70 @@ function changeColor() {
 function setAnswerTile() {
     var answerIndex, min, max;
     min = Math.ceil(0);
-    max = Math.floor(colorTiles.length - 1);
+    max = Math.floor(difficulty); // Num of tiles, math.random has an exclusive ceiling so it has proper indexing
     answerIndex = Math.floor(Math.random() * (max - min)) + min;
     answerRgb = colorTiles[answerIndex].style.backgroundColor;
     currentRgb.textContent = answerRgb.toUpperCase();
     return answerRgb;
 }
 
+function gameOver() {
+    document.querySelector(".jumbotron").style.backgroundColor = answerRgb;
+    winLabel.hidden=false;
+}
+
+// New Colors/New Game Event
 newColors.addEventListener("click", function() {
-    for (i = 0; i < colorTiles.length; i++) {
+	winLabel.hidden=true;
+    for (i = 0; i < difficulty; i++) {
         colorTiles[i].style.backgroundColor = changeColor();
     }
     setAnswerTile();
 });
 
+// easy game event
+easy.addEventListener("click", function() {
+    difficulty = 3; // set the game to easy
+    // hide 3 of tiles, set them to bg #232323
+    for (i = difficulty; i < colorTiles.length; i++) {
+        colorTiles[i].style.backgroundColor = "#232323";
+    }
+    // changeColor()
+    for (i = 0; i < difficulty; i++) {
+        colorTiles[i].style.backgroundColor = changeColor();
+    }
+    // setAnswer
+    setAnswerTile();
+});
 
+// hard game event
+hard.addEventListener("click", function() {
+    difficulty = 6; // set the game to easy
+    // changeColor()
+    for (i = 0; i < difficulty; i++) {
+        colorTiles[i].style.backgroundColor = changeColor();
+    }
+    // hide the 3 of tiles, set them to bg #232323
+    for (i = difficulty; i < colorTiles.length; i++) {
+        colorTiles[i].style.backgroundColor = "#232323";
+    }
+    // setAnswer
+    setAnswerTile();
+});
+
+// Handling the tile clicks
 for (i = 0; i < colorTiles.length; i++) {
     colorTiles[i].addEventListener("click", function() {
         console.log("Clicked on Tile: " + this.style.backgroundColor);
-        if(this.style.backgroundColor===answerRgb){
-        	console.log("YOU WIN");
-        }
-        else{
-        	console.log("WRONG. TRY AGAIN!");
+        if (this.style.backgroundColor === answerRgb) {
+            console.log("YOU WIN");
+            gameOver();
+        } else {
+            console.log("WRONG. TRY AGAIN!");
+            this.style.backgroundColor = "#232323"; // Set bg equal to body bg
         }
     });
 }
+document.addEventListener('DOMContentLoaded', function() {
+    newColors.click();
+}, false);
