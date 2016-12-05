4e03df40a9f90887679da7ccb26c2d3beb6987d9
Rain <rainrivas@gmail.com>
2016-12-04 19:15:57 -0700

    WIP :: Got the basic tiles layout working, new game button works. There is checking put in place to make sure the tiles you click are the right one or the wrong one. Also have it randomly building the tiles and updating the header. Need to add the animation of tiles disappearing when they're the wrong guess as well as a gameover state to stop the game. KNOWN ISSUE : (1) game does not work unless you click new game; the inital page of all purple tiles is useless. (2)the menu links will disappear after being clicked until they're hovered over

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,7 +1,51 @@
 // Temporary decorative js to show it works
-document.querySelector("h1").addEventListener("mouseover",function(){
-	this.style.color="black";
+document.querySelector("h1").addEventListener("mouseover", function() {
+    this.style.color = "black";
 });
-document.querySelector("h1").addEventListener("mouseout",function(){
-	this.style.color="white";
+document.querySelector("h1").addEventListener("mouseout", function() {
+    this.style.color = "white";
 });
+
+var newColors = document.querySelector("#new")
+var colorTiles = document.querySelectorAll(".square");
+var currentRgb = document.querySelector("#currentRgb");
+var answerRgb;
+function changeColor() {
+    var r, g, b, colorRgb, min, max;
+    min = Math.ceil(0);
+    max = Math.floor(255);
+    r = Math.floor(Math.random() * (max - min)) + min;
+    g = Math.floor(Math.random() * (max - min)) + min;
+    b = Math.floor(Math.random() * (max - min)) + min;
+    return colorRgb = "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function setAnswerTile() {
+    var answerIndex, min, max;
+    min = Math.ceil(0);
+    max = Math.floor(colorTiles.length - 1);
+    answerIndex = Math.floor(Math.random() * (max - min)) + min;
+    answerRgb = colorTiles[answerIndex].style.backgroundColor;
+    currentRgb.textContent = answerRgb.toUpperCase();
+    return answerRgb;
+}
+
+newColors.addEventListener("click", function() {
+    for (i = 0; i < colorTiles.length; i++) {
+        colorTiles[i].style.backgroundColor = changeColor();
+    }
+    setAnswerTile();
+});
+
+
+for (i = 0; i < colorTiles.length; i++) {
+    colorTiles[i].addEventListener("click", function() {
+        console.log("Clicked on Tile: " + this.style.backgroundColor);
+        if(this.style.backgroundColor===answerRgb){
+        	console.log("YOU WIN");
+        }
+        else{
+        	console.log("WRONG. TRY AGAIN!");
+        }
+    });
+}
