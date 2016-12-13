a8505d2d3d8ce3878f1eaf12e419ece5a886a565
Rain <rain.rivas@gmail.com>
2016-12-12 21:39:58 -0700

    Selectors exercise for jQuery

---
+++ jQuery/jQuerySelectorsExercise.js
@@ -0,0 +1,5 @@
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+$("#third").css("border","orange solid 4px");
+$("div:first-of-type").css("color","pink");
+// $("div:first") will also work, but it's a bit slower than first-of-type because "first" is not a native css as opposed to "first-of-type"
